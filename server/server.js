e7988eda9f7074c47c13332342d190ebbc6e4845
matteospringmann <matteo.springmann@epitech.eu>
2025-09-30 14:48:36 +0200

    lkjgdfshl

--- server/server.js
+++ server/server.js
@@ -6,12 +6,24 @@ const contactRoutes = require("./routes/contact.routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = ["https://portfoliomatteospringmann.vercel.app/"];
+
 const corsOptions = {
-    origin: "https://portfoliomatteospringmann-matteospringmanns-projects.vercel.app/",
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg =
+                "The CORS policy for this site does not allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     optionsSuccessStatus: 200,
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
